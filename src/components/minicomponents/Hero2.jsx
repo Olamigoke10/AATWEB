c03655d4fe20780faa868144059eb28a1a224a95
MRBOX <olamigokeadebayo90@gmail.com>
2025-09-17 18:00:22 +0100

    Herojsx and navbar

--- src/components/minicomponents/Hero2.jsx
+++ src/components/minicomponents/Hero2.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MoveRight } from 'lucide-react';
-import heroAbout from '../../assets/imgs/heroABout.jpg';
+import heroAbout from '../../../public/assets/imgs/heroAbout.jpg';
 
 const Hero2 = () => {
   return (
