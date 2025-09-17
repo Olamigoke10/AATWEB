d9e0739e3ad6c53693ceea065dd6a667cf4777de
MRBOX <olamigokeadebayo90@gmail.com>
2025-09-17 17:45:51 +0100

    Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Hero1 from '../components/minicomponents/Hero1'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Footer />
 
     </div>
   )
